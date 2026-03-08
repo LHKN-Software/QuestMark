f37157f0bab836066710e4f96d9df7d770278d31
Andrew Lahikainen <lahikainen.andrew@gmail.com>
2026-03-08 20:33:50 +1300

    add comments to markdown static methods

--- QuestMark/Markdown.cs
+++ QuestMark/Markdown.cs
@@ -5,13 +5,31 @@ using QuestMark.Renderers.Styles;
 
 namespace QuestMark;
 
+/// <summary>
+/// Contains methods to transform markdown into <see cref="MarkdownComponent"/>s
+/// </summary>
 public static class Markdown
 {
+    /// <summary>
+    /// Converts given markdown string to a QuestPDF component which can then be inserted into a
+    /// PDF document. Uses default styling options.
+    /// </summary>
+    /// <param name="pipeline">Markdown pipeline</param>
+    /// <param name="markdown">Markdown string</param>
+    /// <returns>A QuestPDF component</returns>
     public static MarkdownComponent ToMarkdownComponent(
         MarkdownPipeline pipeline,
         string markdown
     ) => ToMarkdownComponent(pipeline, markdown, PdfStyleOptions.Default);
 
+    /// <summary>
+    /// Converts given markdown string and PDF styling options to a QuestPDF component which can
+    /// then be inserted into a PDF document.
+    /// </summary>
+    /// <param name="pipeline">Markdown pipeline</param>
+    /// <param name="markdown">Markdown string</param>
+    /// <param name="styleOptions">PDF style options</param>
+    /// <returns>A QuestPDF component</returns>
     public static MarkdownComponent ToMarkdownComponent(
         MarkdownPipeline pipeline,
         string markdown,
